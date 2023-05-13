80ce72d6c65f067a4d0ef165542d3ad746124124
Mehmet Alp Arslan <5alparslanmehmet8@gmail.com>
2023-05-13 14:33:17 +0300

    v8

--- aindex.php
+++ aindex.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="news.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+
     <title>Mehmet Alp Arslan</title>
 </head>
 <body>
